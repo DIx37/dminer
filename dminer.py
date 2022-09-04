b4c0a9f2bb41c466195a9aa37268d6e6cacdf9e2
DIx37 <dkarnov@gmail.com>
2022-09-05 02:23:58 +0300

    2

--- dminer.py
+++ dminer.py
@@ -108,6 +108,9 @@ def read_log_trex(gpu_json):
             gpu_json['number_line_log_trex'] = i
             match_hash = re.findall('GPU #.*: .* - .* MH', str(line))
             print(match_hash)
+        else:
+            print(match_hash)
+            print("Нет новых строк")
     # numner_gpu = {
     #               "0": 0,
     #               "1": 0,
