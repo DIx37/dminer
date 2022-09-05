29a5fea2d4890ea256a13caabdbfb4b58ec29bfc
DIx37 <dkarnov@gmail.com>
2022-09-05 22:39:56 +0300

    0.508

--- dminer.py
+++ dminer.py
@@ -134,6 +134,7 @@ def read_log_gminer(gpu_json):
                 # match_number_gpu = re.findall('#.*:', str(match_hash))[0][1:-1]
                 # match_speed_hash = re.findall(' - .* MH', str(match_hash))[0][3:] + "/s"
                 match_hash = match_hash[0].split()
+                print(match_hash)
                 match_number_gpu = match_hash[1]
                 match_speed_hash = f"{match_hash[3]} {match_hash[4]}/s"
                 gpu_json['GPU'][match_number_gpu]['speed_log_hash'] = match_speed_hash
@@ -150,7 +151,7 @@ def read_log_gminer(gpu_json):
 def screen(gpu_json, log_trex, log_gminer):
     os.system("clear")
 
-    print(f"DIx Miner v0.507    Время: {gpu_json['timestamp']}    Версия драйвера: {gpu_json['driver_version']}    Версия CUDA: {gpu_json['cuda_version']}")
+    print(f"DIx Miner v0.508    Время: {gpu_json['timestamp']}    Версия драйвера: {gpu_json['driver_version']}    Версия CUDA: {gpu_json['cuda_version']}")
 
     td = [
           '№',
